ebe747aead90120b0ecddc2e3ebfe53238a1aabf
Ewerton Mendonca <netrometro@yahoo.com.br>
2020-09-04 15:02:01 -0300

    teste

---
+++ src/main/java/br/mendonca/Teste.java
@@ -0,0 +1,28 @@
+package br.mendonca;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Teste extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public Teste() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println(request.getRequestURI());
+		request.getRequestDispatcher("/views/index.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
